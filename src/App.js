b5a0c58f384622256f964faeb8556f73d5bd24a7
Oğuz Kaan Mertaslan <kmertaslan66@gmail.com>
2022-04-05 23:34:33 +0300

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  const [isDarkMode, setIsDarkMode] = useState(
+    JSON.parse(localStorage.getItem("isDarkMode"))
+  );
+
+  useEffect(() => {
+    isDarkMode
+      ? document.body.classList.add("dark-mode")
+      : localStorage.setItem("isDarkMode", false);
+  }, [isDarkMode]);
+
+  const darkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem("isDarkMode", !isDarkMode);
+    document.body.classList.toggle("dark-mode");
+  };
+  return (
+    <div className="header">
+      <button className="myButton" onClick={() => darkMode()}>
+        Change Theme
+      </button>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
